7ae25349d1e75b8eaefef85254d6d352e8caf375
Máté <matyivan@gmail.com>
2023-10-17 15:36:25 +0200

    First commit.

---
+++ src/main/java/hu/progmasters/fundraiser_own_demo/FundraiserOwnDemoApplication.java
@@ -0,0 +1,13 @@
+package hu.progmasters.fundraiser_own_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FundraiserOwnDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FundraiserOwnDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/hu/progmasters/fundraiser_own_demo/FundraiserOwnDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.progmasters.fundraiser_own_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FundraiserOwnDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
